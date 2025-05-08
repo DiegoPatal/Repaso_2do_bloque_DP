b213fed6b75486905b87a00c8298ffc0c8f4afe6
5to Bachillerato B <5to Bachillerato B@DESKTOP-FF54FBD>
2025-05-08 12:47:48 -0600

    Agregar archivos de proyecto.

---
+++ Inicio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Repaso_2do_bloque_DP
+{
+    public partial class Inicio : Form
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NumeroAleatorio repaso1 = new NumeroAleatorio();
+            repaso1.Show();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AlumnoAleatorio repaso2 = new AlumnoAleatorio();
+            repaso2.Show();
+        }
+    }
+}
---
+++ NumeroAleatorio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Repaso_2do_bloque_DP
+{
+    public partial class NumeroAleatorio : Form
+    {
+        public NumeroAleatorio()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParase(textBox1.Text, out int numMax))
+            {
+
+            }
+        }
+    }
+}
